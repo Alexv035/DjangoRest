5b9a153e3a1c65f464dda7892b9e0379359097e1
Alexv035 <voronkov.a.g@gmail.com>
2022-11-16 13:17:18 +0300

    Lesson 3. 6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AuthorList from './components/Author.js';
+import ProjectList from './components/Project.js';
+
 import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      'authors': []
+      'projects': []
     }
   }
 
   componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/authors')
+    axios.get('http://127.0.0.1:8000/project/projects')
       .then(response => {
         const authors = response.data
         this.setState(
           {
-            'authors': authors
+            'projects': projects
           }
         )
       }).catch(error => console.log(error))
@@ -27,10 +29,44 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <AuthorList authors={this.state.authors} />
+        <ProjectList projects={this.state.projects} />
       </div>
     )
   }
 }
 
 export default App;
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       'authors': [],
+//       'project': []
+//     }
+//   }
+
+//   componentDidMount() {
+//     axios.get('http://127.0.0.1:8000/api/authors')
+//       .then(response => {
+//         const authors = response.data
+//         this.setState(
+//           {
+//             'authors': authors,
+//             'project': project
+//           }
+//         )
+//       }).catch(error => console.log(error))
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <AuthorList authors={this.state.authors} />
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
---
+++ frontend/src/components/Project.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const ProjectItem = ({ project }) => {
+    return (
+        <tr>
+            <td>
+                {author.first_name}
+            </td>
+            <td>
+                {author.last_name}
+            </td>
+            <td>
+                {author.birthday_year}
+            </td>
+        </tr>
+    )
+}
+
+
+const ProjectList = ({ project }) => {
+    return (
+        <table>
+            <th>
+                First name
+            </th>
+            <th>
+                Last Name
+            </th>
+            <th>
+                Birthday year
+            </th>
+            {project.map((project) => <ProjectItem project={project} />)}
+        </table>
+    )
+}
+
+export default ProjectList
