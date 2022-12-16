53cb3a2ff5a5f040be304afe16809f3001285486
Alexv035 <voronkov.a.g@gmail.com>
2022-12-16 20:18:04 +0300

    Lesson 7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 import AuthorList from './components/Author.js';
 import ProjectList from './components/Project.js';
+import LoginForm from './components/Auth.js'
+import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom'
 
 import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+const NotFound404 = ({ location }) => {
+  return (
+    <div>
+      <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+    </div>
+  )
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      'projects': []
+      'projects': [],
+      'token': ''
     }
+  }
+
+  set_token(token) {
+    const cookies = new Cookies()
+    cookies.set('token', token)
+    this.setState({ 'token': token })
+  }
+
+  is_authenticated() {
+    return this.state.token != ''
+  }
+  logout() {
+    this.set_token('')
+  }
+
+  get_token_from_storage() {
+    const cookies = new Cookies()
+    const token = cookies.get('token')
+    this.setState({ 'token': token })
+  }
+
+  get_token(username, password) {
+    axios.post('http://127.0.0.1:8000/api-token-auth/', {
+      username: username,
+      password: password
+    })
+      .then(response => {
+        this.set_token(response.data['token'])
+      }).catch(error => alert('Неверный логин или пароль'))
+  }
+
+  load_data() {
+    axios.get('http://127.0.0.1:8000/api/projects/')
+      .then(response => {
+        this.setState({ authors: response.data })
+      }).catch(error => console.log(error))
   }
 
   componentDidMount() {
@@ -30,6 +78,9 @@ class App extends React.Component {
     return (
       <div>
         <ProjectList projects={this.state.projects} />
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
       </div>
     )
   }
