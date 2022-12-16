59cf1219843bb47ef0d691a620457bad68d6d235
Alexv035 <voronkov.a.g@gmail.com>
2022-12-16 20:35:31 +0300

    Lesson 8. Code style fix

--- todo/tests.py
+++ todo/tests.py
@@ -1,25 +1,26 @@
+import json
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from mixer.backend.django import mixer
+from rest_framework import status
+from rest_framework.test import APIClient, APIRequestFactory, APISimpleTestCase, APITestCase, force_authenticate
+
 from .models import Project
 from .views import ProjectViewSet
-from django.contrib.auth.models import User
-from mixer.backend.django import mixer
-import json
-from django.test import TestCase
-from rest_framework import status
-from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
 
 
 class TestProjectViewSet(TestCase):
     def test_get_list(self):
         factory = APIRequestFactory()
-        request = factory.get('/api/projects/')
-        view = ProjectViewSet.as_view({'get': 'list'})
+        request = factory.get("/api/projects/")
+        view = ProjectViewSet.as_view({"get": "list"})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_guest(self):
         factory = APIRequestFactory()
-        request = factory.post('/api/authors/', {'name': 'Пушкин',
-                                                 'birthday_year': 1799}, format='json')
-        view = ProjectViewSet.as_view({'post': 'create'})
+        request = factory.post("/api/authors/", {"name": "Пушкин", "birthday_year": 1799}, format="json")
+        view = ProjectViewSet.as_view({"post": "create"})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
